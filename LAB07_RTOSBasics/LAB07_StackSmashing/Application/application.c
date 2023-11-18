8f2c6d3c07bf348af8640e5180769529c1394b58
Vikram Damani <vikram.damani@damani.ch>
2023-11-18 18:29:54 +0100

    stack overflow is funny, don't you think?

--- LAB07_RTOSBasics/LAB07_StackSmashing/Application/application.c
+++ LAB07_RTOSBasics/LAB07_StackSmashing/Application/application.c
@@ -9,12 +9,13 @@
 #include "tx_api.h"
 #include "tx_user.h"
 #include <inttypes.h>
+#include <math.h>
 #include <stdio.h>
 
 // ==== Definitions/Globals ====================================================
 
 TX_THREAD thread1;
-#define THREAD1_STACK_SIZE_BYTES 600
+#define THREAD1_STACK_SIZE_BYTES 1024
 ULONG thread1_stack[(THREAD1_STACK_SIZE_BYTES + sizeof(ULONG) - 1) / sizeof(ULONG)];
 
 // ==== THREAD 1 ===============================================================
@@ -29,12 +30,18 @@ uint32_t fib(uint32_t n) {
   //           fib(0) = 0
   //           fib(1) = 1
   //           fib(n) = fib(n-1) + fib(n-2), n >= 2
-  return 0;
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
 }
-
+static uint32_t n = 0;
 static void thread1_entry(ULONG parameter) {
   UNUSED(parameter);
-  for (uint32_t n = 0; n <= 20; n++) {
+  for (; n <= 20; n++) {
     printf("fib(%02" PRIu32 ") = %" PRIu32 "\r\n", n, fib(n));
   }
 }
@@ -44,7 +51,15 @@ static void thread1_entry(ULONG parameter) {
 // TODO Implement a stack-error handler that prints an error message and then
 //      gets stuck in an infinite loop.
 // HINT: The function must return void and take a TX_THREAD pointer as an argument.
-
+__NO_RETURN
+void stack_error_handler(TX_THREAD *ptr) {
+  UNUSED(ptr);
+  float phi = (1 + sqrt(5)) / 2;
+  float fibonacci = round(pow(phi, n) / sqrt(5));
+  printf("LMAO, the stack did overflow xP\r\n(btw, at n = %" PRIu32 ", where fib would've been: %g)\r\nbreh\r\n", n, fibonacci);
+  while (1) {
+  }
+}
 // ==== APPLICATION DEFINE =====================================================
 
 UINT application_define(void) {
@@ -52,6 +67,7 @@ UINT application_define(void) {
   UINT ret = TX_SUCCESS;
 
   // TODO: Register your stack error handler with ThreadX.
+  tx_thread_stack_error_notify(&stack_error_handler);
 
   ret = tx_thread_create(&thread1,                 // Pointer to thread handle.
                          "StackSmasher",           // Thread name.
